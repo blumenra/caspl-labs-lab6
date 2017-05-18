1abdf6cd4425516cad9a563f0536172bc9c04f84
Alon Blumenreich <alon@mail.com>
2017-05-18 12:32:44 +0300

    First commit for task3. Copied all files from lab5

---
+++ task3/main.c
@@ -0,0 +1,36 @@
+
+#include "LineParser.c"
+
+
+int main(int argc, char** argv){
+
+  initialize_shell();
+
+  debugger(argc, argv);
+
+
+  while(1){
+    
+    printCurrentPath();
+
+    char userInput[2048];
+
+    printf(" >> ");
+    fgets(userInput, 2048, stdin);
+
+    if(strcmp(userInput, "quit\n") == 0){
+      freeJobList(jobs);
+      free(initial_tmodes);
+      exit(0);
+    }
+    
+    if(userInput[0] != '\n'){
+
+      cmdLine* cmdLine = parseCmdLines(userInput);
+      printCommandLine(cmdLine);
+      execute(cmdLine);
+    }
+  }
+
+  return 0;
+}
---
+++ task3/test.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	
+	// int i = 0;
+	// while(i < 2000000){
+
+	// 	printf("%d ", i);
+	// 	i++;
+	// }
+
+	puts("first print");
+	sleep(5);
+	puts("second print");
+	sleep(5);
+	puts("third print");
+
+	return 0;
+}
